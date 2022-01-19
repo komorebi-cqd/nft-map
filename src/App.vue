652a0bc9f8badd69309c2ee4b7eb8bf62646d7a6
cqd <you@example.com>
2022-01-19 10:30:08 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Maps from './components/Map4.vue'
+import Maps2 from './components/Map2.vue'
+</script>
+
+<template>
+  <Maps/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Map2.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="maps-container">
+        <div class="main" ref="chartDom2"></div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import * as echarts from "echarts";
+import mapList from "../config/mapList3.json";
+
+const chartDom2 = ref("");
+let myChart;
+
+let option = {
+    legend: {
+        itemWidth: 25,
+        itemHeight: 25,
+        type: 'plain',
+    },
+    
+    aria: {
+        enabled: true,
+    },
+    tooltip: {
+        formatter: function (info) {
+            return [info.name].join("");
+        },
+    },
+    series: [
+        {
+            type: "treemap",
+            levels: [
+                {
+                    itemStyle: {
+                        borderWidth: 3,
+                        // borderColor: "#333",
+                        gapWidth: 3,
+                    },
+                },
+                {
+                    color: ["#942e38", "#aaa", "#269f3c"],
+                    colorMappingBy: "value",
+                    itemStyle: {
+                        gapWidth: 1,
+                    },
+                },
+            ],
+            label: {
+                show: true,
+                formatter: "{b}",
+                ellipsis: true,
+            },
+
+            data: mapList,
+        },
+    ],
+};
+
+// const loopArray = () => {
+//         let arr = [];
+//         for (let i = -19; i <= 19; i++) {
+//                 for (let j = -19; j <= 19; j--) {
+//                         arr.push([i,j,56])
+//                 }
+//         }
+//         return arr;
+// }
+
+// console.dir(loopArray());
+
+onMounted(() => {
+    myChart = echarts.init(chartDom2.value);
+    //     myChart.showLoading();
+
+    option && myChart.setOption(option);
+});
+</script>
+<style>
+.main{
+    width: 800px;
+    height: 800px;
+    border: 1px solid #333;
+    margin: 0 auto;
+}
+</style>
